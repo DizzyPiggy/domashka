c297376fa1022109d2fbc7ef40032c3991ae0557
Timur <raggimovtimur@gmail.com>
2025-09-28 14:16:26 +0300

    Initial commit

---
+++ homework/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //int[] a = { 1, 2, 3 };
+        //Console.WriteLine(a.Sum() / a.Length);
+
+        //int odd = 0;
+        //int even = 0;
+        //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //for (int i = 0; i < a.Length; i++)
+        //{
+        //    if (a[i] % 2 == 0)
+        //    {
+        //        odd++;
+        //    }
+        //    else
+        //    {
+        //        even++;
+        //    }
+        //}
+        //Console.WriteLine($"{odd} нечетных");
+        //Console.WriteLine($"{even} четных");
+
+        //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //Console.WriteLine("Введите число, и я проверю есть ли оно в моем массиве:");
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //if (a.Contains(b))
+        //{
+        //    Console.WriteLine("Это число есть в массиве");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Этого числа нет в массиве");
+        //}
+
+        //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //Array.Reverse(a);
+        //foreach (int i in a)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+
+
+        //var list = new List<int>();
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //while (a != 0)
+        //{
+        //    list.Add(a);
+        //    a = Convert.ToInt32(Console.ReadLine());
+        //}
+        //Console.WriteLine(string.Join(", ", list));
+
+        //var list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //List<int> list1 = new List<int>();
+        //foreach (var i in list)
+        //{
+        //    if (i % 2 != 0)
+        //    {
+        //        list1.Add(i);
+        //    }
+        //}
+        //Console.WriteLine(string.Join(", ", list1));
+
+        //var list = new List<int>() { 3, 6, 1, 8, 9, 4, 10, 5, 2, 7 };
+        //list.Sort();
+        //Console.WriteLine(string.Join(", ", list));
+
+        //var list = new List<int>() { 1, 2, 3, 4, 5 };
+        //var list1 = new List<int>() { 6, 7, 8, 9, 10 };
+        //list.AddRange(list1);
+        //Console.WriteLine(string.Join(", ", list));
+
+        //var list = new List<int>() { 1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 9, 10 };
+        //var list2 = new List<int>();
+        //var list3 = new List<int>();
+        //foreach (var i in list)
+        //{
+        //    if (list3.Contains(i) == false)
+        //    {
+        //        list2.Add(i);
+        //    }
+        //    list3.Add(i);
+        //}
+        //Console.WriteLine(string.Join(", ", list2));
+    }
+}
